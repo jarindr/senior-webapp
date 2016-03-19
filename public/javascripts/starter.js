27c2c187dccc3ead33fa9ff52db44d33619d4d6c
vtno <lynx.tno@gmail.com>
2016-03-19 19:19:20 +0700

    add login form and main page

--- public/javascripts/starter.js
+++ public/javascripts/starter.js
@@ -5,8 +5,8 @@ import 'bootstrap'
 import '../../bower_components/bootstrap/dist/css/bootstrap.css'
 
 import '../javascripts/graph.js'
-$(document).ready(function(){
-  $('#login').click(function() {
+$(document).ready(()=>{
+  $('#login').click(()=>{
     $('.vc-child').load('signin');
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chulalongkorn data center' });
+  res.render('index', { title: 'Chulalongkorn data center' })
 });
 router.get('/signin', function(req, res, next) {
   res.render('signin', {
     title: 'Chulalongkorn data center',
- });
-});
+  })
+})
+router.post('/signin', function(req, res, next){
+  console.log(req.body.username)
+  res.redirect('main')
+})
+
+router.get('/main', function(req, res, next) {
+  res.render('main', {
+    title: 'Chulalongkorn data center',
+  })
+})
 module.exports = router;
