08e86b920e008944ea664bcf89e852c0e37b159f
jarindr <jarindr23@hotmail.com>
2016-03-20 00:32:42 +0700

    ensure db not load

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-require('./database/connection')
+//require('./database/connection')
 var app = express();
 
 // view engine setup
